d17c83fd089e771aa51307ff81eaeceda3b5fcce
alejio <ge02040@hotmail.com>
2019-12-01 19:21:13 +0000

    day 1 complete

--- day_01/src/fuel_calculator.py
+++ day_01/src/fuel_calculator.py
@@ -1,11 +1,22 @@
-from typing import List
+from typing import List, Optional
 import csv
 import os
 import math
+import click
 
 
-def module_fuel_calculator(mass: int) -> int:
-    return math.trunc(mass/3) - 2
+def module_fuel_calculator(mass: int, complex: Optional) -> int:
+
+    def simple_calculation(x: int) -> int:
+        return max(0, math.trunc(x/3) - 2)
+
+    fuel = list()
+    fuel.append(simple_calculation(mass))
+    if complex is not None:
+        while fuel[-1] > 0:
+            fuel_for_fuel = simple_calculation(fuel[-1])
+            fuel.append(fuel_for_fuel)
+    return sum(fuel)
 
 
 def load_input() -> List[int]:
@@ -17,10 +28,13 @@ def load_input() -> List[int]:
         return [int(i[0]) for i in out]
 
 
-def calculate_total_fuel() -> int:
+@click.command()
+@click.option('--complex', default=None)
+def calculate_total_fuel(complex: Optional) -> int:
     module_masses = load_input()
-    return sum([module_fuel_calculator(mass) for mass in module_masses])
+    individual_modules_fuel = [module_fuel_calculator(mass, complex) for mass in module_masses]
+    print(f"Total fuel required: {sum(individual_modules_fuel)}")
 
 
 if __name__ == "__main__":
-    print(calculate_total_fuel())
+    calculate_total_fuel()
