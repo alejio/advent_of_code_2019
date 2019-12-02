01ffc5987b3236eb0b8bd6aa3a2a2dbdb4721362
alejio <ge02040@hotmail.com>
2019-12-02 22:56:26 +0000

    addition

--- day_02/src/intcode_computer.py
+++ day_02/src/intcode_computer.py
@@ -3,6 +3,8 @@ import os
 from typing import List
 import csv
 
+operation_opcodes = {'addition': 1, 'multiplication': 2, 'halt': 99}
+
 
 def load_input() -> List[int]:
     dirname = os.path.dirname(__file__)
@@ -10,6 +12,38 @@ def load_input() -> List[int]:
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         return [int(i) for i in list(reader)[0]]
+
+
+def add(sequence: List[int], integer_list: List[int]) -> List[int]:
+    assert sequence[0] == operation_opcodes['addition']
+    assert len(sequence) == 4
+    integer_list[sequence[3]] = integer_list[sequence[1]] + integer_list[sequence[2]]
+    return integer_list
+
+
+def multiply(sequence: List[int], integer_list: List[int]) -> List[int]:
+    assert sequence[0] == operation_opcodes['multiplication']
+    assert len(sequence) == 4
+    integer_list[sequence[3]] = integer_list[sequence[1]] * integer_list[sequence[2]]
+    return integer_list
+
+
+def halt(sequence: List[int], integer_list: List[int]) -> List[int]:
+    assert sequence[0] == 99
+    assert len(sequence) == 1
+
+
+def intcode_processor(integer_list: List[int]) -> List[int]:
+    return integer_list
+#     processed_list = []
+#     for i in range(len(integer_list)):
+#         if i in halting_opcodes:
+#             return processed_list
+#         else
+#     if integer_list[0] not in valid_opcodes:
+#         raise Exception('Invalid starting opcode')
+#     else:
+
 
 
 def restore_1202_state():
--- day_02/tests/test_intcode_computer.py
+++ day_02/tests/test_intcode_computer.py
@@ -1,4 +1,4 @@
-from day_02.src.intcode_computer import load_input
+from day_02.src.intcode_computer import load_input, add, multiply, halt, intcode_processor
 
 
 def test_load_input():
@@ -6,3 +6,19 @@ def test_load_input():
     assert len(out) == 137
     assert out[0] == 1
     assert out[-1] == 0
+
+
+def test_add():
+    integer_list = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+    sequence = integer_list[0:4]
+    add(sequence, integer_list) == [1, 1, 1, 4, 2, 5, 6, 0, 99]
+
+def test_intcode_processor():
+    case_1 = [[1, 0, 0, 0, 99], [2, 0, 0, 0, 99]]
+    case_2 = [[2, 3, 0, 3, 99], [2, 3, 0, 6, 99]]
+    case_3 = [[2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]]
+    case_4 = [[1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]]
+    assert intcode_processor(case_1[0]) == case_1[1]
+    assert intcode_processor(case_2[0]) == case_2[1]
+    assert intcode_processor(case_3[0]) == case_3[1]
+    assert intcode_processor(case_4[0]) == case_4[1]
