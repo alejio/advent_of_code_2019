1738a43f82a23f87ce63764c887f4065b38296d9
Alex Spanos <alex@truelayer.com>
2019-12-03 17:48:46 +0000

    move command

--- day_03/src/crossed_wires.py
+++ day_03/src/crossed_wires.py
@@ -9,3 +9,22 @@ def load_input() -> List[int]:
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         return list(reader)
+
+
+def move(movement: str, starting_position: List=[0, 0]) -> List:
+    assert len(starting_position) == 2
+    assert isinstance(starting_position[0], int)
+    assert isinstance(starting_position[1], int)
+    distance = int(movement[1:])
+    direction = movement[0]
+    if direction == 'R':
+        return [starting_position[0], starting_position[1] + distance]
+    elif direction == 'L':
+        return [starting_position[0], starting_position[1] - distance]
+    elif direction == 'U':
+        return [starting_position[0] + distance, starting_position[1]]
+    elif direction == 'D':
+        return [starting_position[0] - distance, starting_position[1]]
+    else:
+        print('Wrong direction')
+
--- day_03/tests/test_crossed_wires.py
+++ day_03/tests/test_crossed_wires.py
@@ -1,4 +1,4 @@
-from day_03.src.crossed_wires import load_input
+from day_03.src.crossed_wires import load_input, move
 
 
 def test_load_input():
@@ -8,3 +8,8 @@ def test_load_input():
     assert len(out[1]) == 301
     assert out[0][0] == 'R1003'
     assert out[1][-1] == 'R261'
+
+
+def test_move():
+    starting_position = [0, 1]
+    assert move('U42', starting_position) == [42, 1]
