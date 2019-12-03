93873f87f9ff4053175ee8d61e924a878bca4dff
alejio <ge02040@hotmail.com>
2019-12-03 23:49:44 +0000

    got part 1

--- day_03/src/crossed_wires.py
+++ day_03/src/crossed_wires.py
@@ -31,31 +31,55 @@ def move(movement: str, starting_position: Tuple=(0, 0)) -> Tuple:
         print('Wrong direction')
 
 
+def single_move_map(starting_position: Tuple, ending_position: Tuple) -> List:
+    diff_x = starting_position[0] - ending_position[0]
+    diff_y = starting_position[1] - ending_position[1]
+    path = list()
+    if diff_x < 0:
+        for i in range(-diff_x + 1):
+            path.append((starting_position[0] + i, starting_position[1]))
+    elif diff_x > 0:
+        for i in range(diff_x + 1):
+            path.append((starting_position[0] - i, starting_position[1]))
+    else:
+        if diff_y < 0:
+            for i in range(-diff_y + 1):
+                path.append((starting_position[0], starting_position[1] + i))
+        elif diff_y > 0:
+            for i in range(diff_y + 1):
+                path.append((starting_position[0], starting_position[1] - i))
+        else:
+            path.append((starting_position[0], starting_position[1]))
+    return path
+
+
 def manhattan_distance(position_1: Tuple, position_2: Tuple) -> int:
     return abs(position_1[0] - position_2[0]) + abs(position_1[1] - position_2[1])
 
 
-def map_path(movements: List[str], starting_position: Tuple=(0, 0)) -> set:
-    path = set()
+def map_full_path(movements: List[str], starting_position: Tuple=(0, 0)) -> List:
+    path = list()
     position = starting_position
     for movement in movements:
-        position = move(movement, position)
-        path.add(position)
+        new_position = move(movement, position)
+        single_path = single_move_map(position, new_position)
+        position = new_position
+        path = path + single_path
     return path
 
 
-def find_intersections(first_path: set, second_path: set) -> set:
-    return first_path.intersection(second_path)
+def find_intersections(first_path: List, second_path: List) -> set:
+    return set(first_path).intersection(set(second_path))
 
 
 @click.command()
 def find_closest_crossing():
     wire_1, wire_2 = load_input()
-    path_1 = map_path(wire_1)
-    path_2 = map_path(wire_2)
+    path_1 = map_full_path(wire_1)
+    path_2 = map_full_path(wire_2)
     intersections = find_intersections(path_1, path_2)
     distances = [manhattan_distance((0, 0), i) for i in list(intersections)]
-    print(min(distances))
+    print(min([i for i in distances if i > 0]))
 
 
 if __name__ == "__main__":
--- day_03/tests/test_crossed_wires.py
+++ day_03/tests/test_crossed_wires.py
@@ -1,5 +1,4 @@
-from day_03.src.crossed_wires import load_input, move, manhattan_distance, \
-    map_path, find_intersections
+from day_03.src.crossed_wires import load_input, move, manhattan_distance, find_intersections, single_move_map
 
 
 def test_load_input():
@@ -19,6 +18,14 @@ def test_move():
     assert move('L42', starting_position) == (0, -41)
 
 
+def test_single_move_map():
+    assert single_move_map((0, 1), (0, 5)) == [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5)]
+    assert single_move_map((0, 5), (0, 1)) == [(0, 5), (0, 4), (0, 3), (0, 2), (0, 1)]
+    assert single_move_map((1, 0), (5, 0)) == [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]
+    assert single_move_map((5, 0), (1, 0)) == [(5, 0), (4, 0), (3, 0), (2, 0), (1, 0)]
+    assert single_move_map((0, 0), (0, 0)) == [(0, 0)]
+
+
 def test_manhattan_distance():
     assert manhattan_distance((4, 2), (7, 1)) == 4
     movements = ['R75','D30','R83','U83','L12','D49','R71','U7','L72', 'U62','R66','U55','R34','D71','R55','D58','R83']
@@ -27,9 +34,9 @@ def test_manhattan_distance():
         position = move(movement, position)
     assert manhattan_distance(position, (0, 0)) == 159
 
-def test_map_path():
-    movements = ['R10', 'U11', 'D1', 'L10']
-    assert map_path(movements) == {(0, 10), (11, 10), (10, 10), (10, 0)}
+# def test_map_path():
+#     movements = ['R10', 'U11', 'D1', 'L10']
+#     assert map_path(movements) == {(0, 10), (11, 10), (10, 10), (10, 0)}
 
 
 def test_find_intersections():
