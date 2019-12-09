2deaf7c18c639684aa2d35f782f2131432c60f09
alejio <ge02040@hotmail.com>
2019-12-09 23:33:51 +0000

    interim commit day 08

--- day_08/src/space_image.py
+++ day_08/src/space_image.py
@@ -1,6 +1,9 @@
 import csv
 import os
-from typing import List
+from typing import List, Dict
+import collections
+
+import click
 
 
 def parser(input_text: str) -> List:
@@ -17,8 +20,37 @@ def load_input() -> List[int]:
 
 def create_image(data: List, dim_x: int, dim_y: int) -> List:
     output = []
-    n_layers = len(data) / (dim_x * dim_y)
+    layer_size = dim_x * dim_y
+    n_layers = int(len(data) / (dim_x * dim_y))
     for n_layer in range(n_layers):
-        layer = [data[0:(dim_x+1)], data[dim_x+1:dim_x+1+dim_y+1]]
-
+        layer_data = data[n_layer*layer_size:n_layer*layer_size + layer_size]
+        layer = [layer_data[0: dim_x], layer_data[dim_x:]]
+        output.append(layer)
     return output
+
+
+def count_digits_in_layer(layer: List, digit: int) -> Dict:
+    flat_layer = sum(layer, [])
+    counter = collections.Counter(flat_layer)
+    return counter[digit]
+
+
+def find_fewest_zeros_layer(image: List) -> List:
+    zeros = {}
+    for idx, layer in enumerate(image):
+        zeros[idx] = count_digits_in_layer(layer, 0)
+    return min(zeros, key=zeros.get)
+
+
+@click.command()
+def calculation():
+    data = load_input()
+    image = create_image(data, 25, 6)
+    layer_idx = find_fewest_zeros_layer(image)
+    number_of_1s = count_digits_in_layer(image[layer_idx], 1)
+    number_of_2s = count_digits_in_layer(image[layer_idx], 2)
+    print(number_of_1s * number_of_2s)
+
+
+if __name__ == '__main__':
+    calculation()
--- day_08/tests/test_space_image.py
+++ day_08/tests/test_space_image.py
@@ -1,4 +1,4 @@
-from day_08.src.space_image import load_input, parser, create_image
+from day_08.src.space_image import load_input, parser, create_image, find_fewest_zeros_layer
 
 
 def test_parser():
@@ -15,5 +15,16 @@ def test_create_image():
     data = parser(raw_data)
     image = create_image(data, 3, 2)
     assert len(image) == 2
-    assert image[0] == ((1, 2, 3), (4, 5, 6))
-    assert image[1] == ((7, 8, 9), (0, 1, 2))
+    assert image[0] == [[1, 2, 3], [4, 5, 6]]
+    assert image[1] == [[7, 8, 9], [0, 1, 2]]
+
+
+def test_find_fewest_zeros_layer():
+    image = [[[1, 0, 3], [0, 0, 4], [0, 0, 0]], [[1, 0, 2], [2, 2, 2], [3, 3, 3]], [[1, 1, 2], [2, 2, 2], [3, 3, 3]]]
+    layer_idx = find_fewest_zeros_layer(image)
+    assert layer_idx == 2
+
+# def test_find_fewest_zeros_layer_tie():
+#     image = [[[1, 0, 2], [2, 2, 2], [3, 3, 3]], [[1, 1, 2], [2, 0, 2], [3, 3, 3]]]
+#     layer_idx = find_fewest_zeros_layer(image)
+#     assert layer_idx == 1
