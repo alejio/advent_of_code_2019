eb6ec6a15105cb47148817b089a4a4f220ce2e89
alejio <ge02040@hotmail.com>
2019-12-09 17:55:32 +0000

    interim commit day 08

--- day_08/src/space_image.py
+++ day_08/src/space_image.py
@@ -3,9 +3,22 @@ import os
 from typing import List
 
 
+def parser(input_text: str) -> List:
+    return [int(i) for i in input_text]
+
+
 def load_input() -> List[int]:
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'input.txt')
     with open(filename, 'r') as f:
         reader = csv.reader(f)
-        return [i for i in list(reader)[0][0]]
+        return parser(list(reader)[0][0])
+
+
+def create_image(data: List, dim_x: int, dim_y: int) -> List:
+    output = []
+    n_layers = len(data) / (dim_x * dim_y)
+    for n_layer in range(n_layers):
+        layer = [data[0:(dim_x+1)], data[dim_x+1:dim_x+1+dim_y+1]]
+
+    return output
--- day_08/tests/test_space_image.py
+++ day_08/tests/test_space_image.py
@@ -1,6 +1,19 @@
-from day_08.src.space_image import load_input
+from day_08.src.space_image import load_input, parser, create_image
+
+
+def test_parser():
+    assert parser('123456789012') == [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
 
 
 def test_load_input():
     data = load_input()
     assert len(data) % (25 * 6) == 0
+
+
+def test_create_image():
+    raw_data = '123456789012'
+    data = parser(raw_data)
+    image = create_image(data, 3, 2)
+    assert len(image) == 2
+    assert image[0] == ((1, 2, 3), (4, 5, 6))
+    assert image[1] == ((7, 8, 9), (0, 1, 2))
