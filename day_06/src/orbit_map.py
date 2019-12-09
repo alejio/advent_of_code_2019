49ec3781065ff464bd680adbb781baf4f3e726b5
alejio <ge02040@hotmail.com>
2019-12-09 12:12:31 +0000

    interim commit day 06

--- day_06/src/orbit_map.py
+++ day_06/src/orbit_map.py
@@ -2,6 +2,8 @@ import csv
 import os
 from typing import List, Dict
 
+import click
+
 
 def load_input() -> List[int]:
     dirname = os.path.dirname(__file__)
@@ -23,3 +25,42 @@ def create_graph() -> Dict[str, List[str]]:
         else:
             graph[pair[0]] = pair[1:]
     return graph
+
+
+def find_all_paths(graph: Dict[str, List[str]], start: str, end: str, path: List=[]) -> List:
+    # Adapted from https://www.python.org/doc/essays/graphs/
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+
+def max_distance(paths: List) -> int:
+    for path in paths:
+        if len(path) > 0:
+            return len(path) - 1
+        else:
+            return 0
+
+
+@click.command()
+def total_orbits():
+    graph = create_graph(load_input())
+    total_distance = 0
+    for node in graph:
+        paths = find_all_paths(graph, 'COM', node)
+        total_distance += max_distance(paths)
+    print(total_distance)
+    return total_distance
+
+
+if __name__ == '__main__':
+    total_orbits()
--- day_06/tests/test_orbit_map.py
+++ day_06/tests/test_orbit_map.py
@@ -1,4 +1,4 @@
-from day_06.src.orbit_map import load_input, create_graph
+from day_06.src.orbit_map import load_input, create_graph, find_all_paths, max_distance
 
 
 def test_load_input():
@@ -8,3 +8,35 @@ def test_load_input():
 def test_create_graph():
     graph = create_graph()
     assert len(graph.keys()) == 2349
+
+
+def test_find_all_paths_case1():
+    graph = {'A': ['B', 'C']}
+    paths = find_all_paths(graph, 'A', 'B')
+    assert len(paths) == 1
+    assert len(paths[0]) == 2
+
+
+def test_find_all_paths_case2():
+    graph = {'A': ['B', 'C'], 'C': ['E']}
+    paths = find_all_paths(graph, 'A', 'E')
+    assert len(paths) == 1
+    assert len(paths[0]) == 3
+
+
+def test_find_all_paths_case3():
+    graph = {'A': ['B', 'C', 'D'], 'C': ['E'], 'D': ['E']}
+    paths = find_all_paths(graph, 'A', 'E')
+    assert len(paths) == 2
+    assert len(paths[0]) == 3
+
+
+def test_max_distance():
+    paths = [['A', 'D', 'E'], ['A', 'B']]
+    assert max_distance(paths) == 2
+
+
+def test_integration():
+    graph = create_graph()
+    paths = find_all_paths(graph, 'COM', '62T')
+    assert len(paths) == 1
