ccce878443f35c2677f83032ce88afab18d23213
alejio <ge02040@hotmail.com>
2019-12-09 10:41:33 +0000

    day 5 incomplete

---
+++ day_05/src/sunny_with_a_chance.py
@@ -0,0 +1,5 @@
+from typing import List
+
+from day_02.src.program_alarm import add, load_input, multiply, halt
+
+operation_opcodes = {'addition': 1, 'multiplication': 2, 'halt': 99, 'save': 3, 'output': 4}
