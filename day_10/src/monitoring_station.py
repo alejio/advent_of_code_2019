897298635f76d9acd3ba99a3429848416a28dbf0
alejio <ge02040@hotmail.com>
2019-12-10 22:40:04 +0000

    interim commit going to sleep

--- day_10/src/monitoring_station.py
+++ day_10/src/monitoring_station.py
@@ -23,6 +23,26 @@ def get_asteroid_positions(data: List) -> List:
     return asteroid_positions
 
 
-
-def count_asteroids(map: List) -> int:
+def blocked_view(point: Tuple, asteroid: Tuple, map_grid: Tuple) -> List:
+    assert point != asteroid, 'Wrong input - same points!'
+    if point[0] == asteroid[0]:
+        if point[1] < asteroid[1]:
+            return [(point[0], j) for j in range(asteroid[1] + 1, map_grid[1] + 1)]
+        return [(point[0], j) for j in range(0, asteroid[1])]
+    if point[1] == asteroid[1]:
+        if point[0] < asteroid[0]:
+            return [(i, point[1]) for i in range(asteroid[0] + 1, map_grid[0] + 1)]
+        return [(i, point[1]) for i in range(0, asteroid[0])]
+    if (point[0] != asteroid[0]) and (point[1] != asteroid[1]):
+        midpoint = (point[0] + asteroid[0]) / 2, (point[1] + asteroid[1]) / 2
+    interval_x = midpoin
     return
+
+
+def count_visible_asteroids(idx: int, asteroids: List, map_grid: Tuple) -> int:
+    station = asteroids[idx]
+    for asteroid in asteroids:
+        if asteroid != station:
+
+    visible_asteroids = 0
+    return visible_asteroids
--- day_10/tests/test_monitoring_station.py
+++ day_10/tests/test_monitoring_station.py
@@ -1,4 +1,4 @@
-from day_10.src.monitoring_station import load_input, get_dimensions, get_asteroid_positions
+from day_10.src.monitoring_station import load_input, get_dimensions, get_asteroid_positions, count_visible_asteroids
 
 
 def test_load_input():
@@ -23,5 +23,9 @@ def test_get_asteroid_positions():
             '....#',
             '...##']
     assert get_asteroid_positions(data) == [(0, 1), (0, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 4), (4, 3), (4, 4)]
-#
-# def test_count_asteroids():
+
+
+def test_count_visible_asteroids():
+    grid = (5, 5)
+    map = [(0, 1), (0, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 4), (4, 3), (4, 4)]
+    assert count_visible_asteroids(8, map, grid) == 8
